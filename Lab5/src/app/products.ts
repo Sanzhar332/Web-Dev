134bda51547a0e92f7aea6ee029200c23020b4ea
Nadyrkhanov Sanzhar <120048849+Sanzhar332@users.noreply.github.com>
2023-03-01 20:03:12 +0600

    add lab 5

---
+++ Lab5/src/app/products.ts
@@ -0,0 +1,338 @@
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone 12 Pro Max',
+    price: 1442,
+    description: 'Wireless carrier\tUnlocked\n' +
+        'Brand\tApple\n' +
+        'Memory storage capacity\t128 GB\n' +
+        'Operating system\tIOS 14\n' +
+        'Colour\tGraphit\n' +
+        'Model year\t2020',
+    rating:4.2,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb3/h80/33277891280926/apple-iphone-12-pro-max-128gb-seryj-100658004-1-Container.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-12-pro-max-128gb-seryi-100658004/?c=750000000#!/item',
+    prodcategory: "1",
+    shows: true,
+    likes:0
+  },
+  {
+    id: 2,
+    name: 'iPhone 11 ',
+    price: 680,
+    description: 'Model name\tApple iPhone 11 128 GB Black\n' +
+        'Wireless carrier\tUnlocked\n' +
+        'Brand\tApple\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t128 GB\n' +
+        'Operating system\tIOS',
+    rating:4.1,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h0a/h2e/31489167622174/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item',
+    shows: true,
+    prodcategory: 1,
+    likes:0
+  },
+  {
+    id: 3,
+    name: 'iPhone XS',
+    price: 782,
+    description: 'Model name\tApple iPhone XS 64GB\n' +
+        'Wireless carrier\tUnlocked\n' +
+        'Brand\tApple\n' +
+        'Form factor\tSmartphone\n' +
+        'Memory storage capacity\t64 GB\n' +
+        'Operating system\tIOS',
+    rating:4,
+    imageSrc: 'https://m.media-amazon.com/images/I/512F7mwDVyL._AC_SL1024_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Apple-iPhone-XS-Refurbished-64GB/dp/B07N9HM3Z4/ref=sr_1_1?crid=ULNFHA2GVAWW&keywords=iphone+XS&qid=1646431885&sprefix=iphone+xs%2Caps%2C160&sr=8-1',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 4,
+    name: 'iPhone 13',
+    price: 852,
+    description: 'Model name\tApple iPhone 13 128 GB Blue\n' +
+        'Wireless carrier\tUnlocked\n' +
+        'Brand\tApple\n' +
+        'Form factor\tSmartphone\n' +
+        'Memory storage capacity\t128 GB\n' +
+        'Operating system\tIOS',
+    rating:4.3,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h42/hed/46392661704734/apple-iphone-13-128gb-sinij-102298364-1-Container.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-sinii-102298364/?c=750000000#!/item',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 5,
+    name: 'iPhone 14 Pro Max ',
+    price: 1589,
+    description: 'Model name\tIPhone 14 Pro Max Purple\n' +
+        'Wireless carrier\tUnlocked\n' +
+        'Brand\tApple\n' +
+        'Form factor\tSmartphone\n' +
+        'Memory storage capacity\t256 GB\n' +
+        'Operating system\tIOS',
+    rating:4.0,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1a/h61/62948780736542/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item',
+    prodcategory: 1,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 8,
+    name: 'Samsung Galaxy A12',
+    price: 201,
+    description: 'Display: 6,5 Zoll (16,63 cm)\n' +
+        'Hauptkamera: 48 + 5 + 2 + 2 MP\n' +
+        'Speicher: 128 GB / 4 GB RAM\n' +
+        'Akku: 5000 mAh',
+    rating:4.5,
+    imageSrc: 'https://m.media-amazon.com/images/I/91akbfdbByL._AC_SL1500_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-128GB-Mobile-Android/dp/B08Q8GJXPF/ref=sr_1_2?crid=2ZF9UC4U7J8EN&keywords=samsung&qid=1646432461&sprefix=samsun%2Caps%2C189&sr=8-2',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 9,
+    name: 'Samsung Galaxy S20',
+    price: 454,
+    description: 'Model name\tSamsung Galaxy S20 FE\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tSamsung\n' +
+        'Form factor\tBar\n' +
+        'Memory storage capacity\t6 GB\n' +
+        'Operating system\tAndroid 11.0',
+    rating:4.5,
+    imageSrc: 'https://m.media-amazon.com/images/I/61GbkDVtLwL._AC_SL1100_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-Smartphone-Cloud-Android/dp/B096G5MZ34/ref=sr_1_18?crid=2ZF9UC4U7J8EN&keywords=samsung&qid=1646432550&sprefix=samsun%2Caps%2C189&sr=8-18',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 10,
+    name: 'Samsung Galaxy S21',
+    price: 772,
+    description: 'Model name\tR9\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tSamsung\n' +
+        'Form factor\tBar\n' +
+        'Memory storage capacity\t6 GB\n' +
+        'Operating system\tAndroid 12',
+    rating:4.4,
+    imageSrc: 'https://m.media-amazon.com/images/I/81u8oHzGLmL._AC_SL1500_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-Smartphone-Phantom-Android/dp/B08QX85YKM/ref=sr_1_17?crid=2ZF9UC4U7J8EN&keywords=samsung&qid=1646432550&sprefix=samsun%2Caps%2C189&sr=8-17',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 11,
+    name: 'Samsung Galaxy A32',
+    price: 272,
+    description: 'Model name\tSamsung Galaxy A32\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tSamsung\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity',
+    rating:4.5,
+    imageSrc: 'https://m.media-amazon.com/images/I/71xetlaYOTL._AC_SL1500_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-Smartphone-128GB-Black/dp/B08W1V1226/ref=sr_1_11?crid=1OSCH0XCHFZXF&keywords=samsung&qid=1646857726&sprefix=samsung%2Caps%2C214&sr=8-11',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 12,
+    name: 'Samsung Galaxy A22',
+    price: 216,
+    description: 'Model name\tGalaxy A22\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tSamsung\n' +
+        'Form factor\tSmartphone\n' +
+        'Memory storage capacity\t64 GB',
+    rating:4.5,
+    imageSrc: 'https://m.media-amazon.com/images/I/71zAlS6+6rL._AC_SL1500_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-A225-Black-Dual/dp/B0937JCNXT/ref=sr_1_2?crid=309KM17H57HSZ&keywords=samsung+galaxy+a22&qid=1646857796&sprefix=samsung+galaxy+a2%2Caps%2C173&sr=8-2',
+    prodcategory: 2,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 13,
+    name: 'Xiaomi Poco M3 Pro',
+    price: 218,
+    description: 'Model name\tPoco M3 Pro 5G\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tXiaomi\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t4 GB',
+    rating:4.5,
+    imageSrc: 'https://m.media-amazon.com/images/I/518-LW36OCS._AC_SL1001_.jpg',
+    KaspiLink: 'https://m.media-amazon.com/images/I/518-LW36OCS._AC_SL1001_.jpg',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 14,
+    name: 'Xiaomi Poco X3',
+    price: 293,
+    description: 'Model name\tPoco X3\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tXiaomi\n' +
+        'Form factor\tBlock\n' +
+        'Memory storage capacity\t8 GB',
+    rating:4.6,
+    imageSrc: 'https://m.media-amazon.com/images/I/71fdWd4SqFL._AC_SL1380_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Xiaomi-Poco-X3-Pro-Smartphone/dp/B08Z84SB3R/ref=sr_1_15?crid=3RJ6OMFYB044C&keywords=xiaomi&qid=1646857971&sprefix=xiaomi%2Caps%2C173&sr=8-15&th=1',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 15,
+    name: 'Xiaomi Redmi Note 11',
+    price: 283,
+    description: '\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tXiaomi\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t128 GB\n' +
+        'Colour\tStar Blue',
+    rating:5,
+    imageSrc: 'https://m.media-amazon.com/images/I/51-JEJPrRDL._AC_SL1000_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Xiaomi-Smartphone-DotDisplay-Snapdragon-Camera-Star-Blue/dp/B09QYK466Y/ref=sr_1_2?crid=2NU0ZUOS1LL83&keywords=xiaomi+redmi+note+11&qid=1646857987&sprefix=xiaomi+redmi+note+11%2Caps%2C170&sr=8-2',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 16,
+    name: 'Xiaomi Redmi Note 10',
+    price: 212,
+    description: 'Model name\tRedmi Note 10\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tXiaomi\n' +
+        'Form factor\tSchieberegler\n' +
+        'Memory storage capacity\t128 GB',
+    rating:4.6,
+    imageSrc: 'https://m.media-amazon.com/images/I/71CcXhAu9kS._AC_SL1500_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Xiaomi-Redmi-Note-10-Chrome/dp/B095BLF64H/ref=sr_1_1?crid=39JABBJIWFLUI&keywords=xiaomi%2Bremi%2Bnote%2B10&qid=1646858006&sprefix=xiaomi%2Bredmi%2Bnote%2B10%2Caps%2C171&sr=8-1&th=1',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 17,
+    name: 'Xiaomi Redmi 9C',
+    price: 151,
+    description: 'Model name\tRedmi 9C\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tXiaomi\n' +
+        'Form factor\tSmartphone\n' +
+        'Memory storage capacity\t3 GB\n' +
+        'Operating system\tAndroid 10.0',
+    rating:4.4,
+    imageSrc: 'https://m.media-amazon.com/images/I/81DvJbWniDL._AC_SL1500_.jpg',
+    KaspiLink: 'hhttps://www.amazon.de/-/en/MZB07Q0EU-Xiaomi-Redmi-9C-Smartphone/dp/B08DVHL2DV/ref=sr_1_1?crid=3RJ6OMFYB044C&keywords=xiaomi&qid=1646857971&sprefix=xiaomi%2Caps%2C173&sr=8-1&th=1',
+    prodcategory: 3,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 18,
+    name: 'HTC Desire 500',
+    price: 326,
+    description: 'Wireless carrier\tUnlocked\n' +
+        'Brand\tHTC\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t8 GB\n' +
+        'Operating system\tAndroid 4.2',
+    rating:3,
+    imageSrc: 'https://m.media-amazon.com/images/I/41j9oKsdLkL._AC_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Desire-Unlocked-Smartphone-Android-Jelly/dp/B00E4TLZ4I/ref=sr_1_4?keywords=htc%2Bsmartphone&qid=1646858423&sprefix=htc%2Bsmar%2Caps%2C158&sr=8-4&th=1',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 19,
+    name: 'HTC One M9',
+    price: 200,
+    description: 'Model name\tHTC One M9\n' +
+        'Wireless carrier\tUnlocked\n' +
+        'Brand\tHTC\n' +
+        'Form factor\tStandard (Bar)\n' +
+        'Memory storage capacity\t32 GB',
+    rating:4.1,
+    imageSrc: 'https://m.media-amazon.com/images/I/71K4WAf9vnL._AC_SL1500_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/HTC-One-M9-Gold/dp/B00TX5QRAA/ref=sr_1_8?keywords=htc+smartphone&qid=1646858423&sprefix=htc+smar%2Caps%2C158&sr=8-8',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 20,
+    name: 'HTC Desire 21 Pro',
+    price: 319,
+    description: 'Model name\tDesire 21 Pro 5G\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tHTC\n' +
+        'Form factor\tStoßabsorbierend\n' +
+        'Memory storage capacity\t6 GB',
+    rating:4.2,
+    imageSrc: 'https://m.media-amazon.com/images/I/41n3VHQBxtL._AC_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/HTC-Desire-21-Pro-5G-blue/dp/B092JP9YWH/ref=sr_1_1?keywords=htc+smartphone&qid=1646858384&sprefix=htc+smar%2Caps%2C158&sr=8-1',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 21,
+    name: 'HTC Desire X',
+    price: 104,
+    description: 'Model name\tX\n' +
+        'Wireless carrier\tNET\n' +
+        'Brand\tHTC\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t4096 MB',
+    rating:3.7,
+    imageSrc: 'https://m.media-amazon.com/images/I/71vK8U+4yaL._AC_SL1456_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Desire-Smartphone-Android-Sense-Bluetooth/dp/B009GGY852/ref=sr_1_2?keywords=htc+smartphone&qid=1646858384&sprefix=htc+smar%2Caps%2C158&sr=8-2',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  },
+  {
+    id: 22,
+    name: 'HTC Desire 816',
+    price: 157,
+    description: 'Model name\tHTC Desire 816\n' +
+        'Brand\tHTC\n' +
+        'Form factor\tStandard (Bar)\n' +
+        'Memory storage capacity\t8 GB\n' +
+        'Operating system\tAndroid 4.4\n' +
+        'Colour\tWeiß',
+    rating:4.6,
+    imageSrc: 'https://m.media-amazon.com/images/I/61oiYM+h4gL._AC_SL1000_.jpg',
+    KaspiLink: 'https://www.amazon.de/-/en/Smartphone-Megapixel-Touchscreen-Quad-Core-Processor/dp/B00ISUSB3M/ref=sr_1_3?keywords=htc+smartphone&qid=1646858384&sprefix=htc+smar%2Caps%2C158&sr=8-3',
+    prodcategory: 4,
+    shows: true,
+    likes:0
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
