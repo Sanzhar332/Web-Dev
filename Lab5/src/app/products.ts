64874ef853260172624e2f76bc0243e83e019094
Nadyrkhanov Sanzhar <120048849+Sanzhar332@users.noreply.github.com>
2023-03-05 18:07:28 +0600

    refresh

--- Lab5/src/app/products.ts
+++ Lab5/src/app/products.ts
@@ -1,6 +1,6 @@
 export const products = [
   {
-    id: 1,
+    id: 4,
     name: 'iPhone 12 Pro Max',
     price: 1442,
     description: 'Wireless carrier\tUnlocked\n' +
@@ -17,7 +17,7 @@ export const products = [
     likes:0
   },
   {
-    id: 2,
+    id: 5,
     name: 'iPhone 11 ',
     price: 680,
     description: 'Model name\tApple iPhone 11 128 GB Black\n' +
@@ -34,24 +34,24 @@ export const products = [
     likes:0
   },
   {
-    id: 3,
-    name: 'iPhone XS',
-    price: 782,
-    description: 'Model name\tApple iPhone XS 64GB\n' +
+    id: 2,
+    name: 'iPhone 13 Pro Max',
+    price: 1602,
+    description: 'Model name\tApple iPhone 13 Pro Max 256 GB Green\n' +
         'Wireless carrier\tUnlocked\n' +
         'Brand\tApple\n' +
         'Form factor\tSmartphone\n' +
-        'Memory storage capacity\t64 GB\n' +
+        'Memory storage capacity\t256 GB\n' +
         'Operating system\tIOS',
-    rating:4,
-    imageSrc: 'https://m.media-amazon.com/images/I/512F7mwDVyL._AC_SL1024_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Apple-iPhone-XS-Refurbished-64GB/dp/B07N9HM3Z4/ref=sr_1_1?crid=ULNFHA2GVAWW&keywords=iphone+XS&qid=1646431885&sprefix=iphone+xs%2Caps%2C160&sr=8-1',
+    rating:4.7,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h46/he3/49319418560542/apple-iphone-13-pro-max-256gb-zelenyj-104079153-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/apple-iphone-13-pro-max-256gb-zelenyi-104079153/?c=750000000#!/item',
     prodcategory: 1,
     shows: true,
     likes:0
   },
   {
-    id: 4,
+    id: 3,
     name: 'iPhone 13',
     price: 852,
     description: 'Model name\tApple iPhone 13 128 GB Blue\n' +
@@ -68,7 +68,7 @@ export const products = [
     likes:0
   },
   {
-    id: 5,
+    id: 1,
     name: 'iPhone 14 Pro Max ',
     price: 1589,
     description: 'Model name\tIPhone 14 Pro Max Purple\n' +
@@ -86,244 +86,244 @@ export const products = [
   },
   {
     id: 8,
-    name: 'Samsung Galaxy A12',
-    price: 201,
-    description: 'Display: 6,5 Zoll (16,63 cm)\n' +
+    name: 'Samsung Galaxy A23',
+    price: 206,
+    description: 'Display: 6,6 inch \n' +
         'Hauptkamera: 48 + 5 + 2 + 2 MP\n' +
-        'Speicher: 128 GB / 4 GB RAM\n' +
-        'Akku: 5000 mAh',
+        'Speicher: 128 GB / 6 GB RAM\n' +
+        'Akku: 5000 mAh', 
     rating:4.5,
-    imageSrc: 'https://m.media-amazon.com/images/I/91akbfdbByL._AC_SL1500_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-128GB-Mobile-Android/dp/B08Q8GJXPF/ref=sr_1_2?crid=2ZF9UC4U7J8EN&keywords=samsung&qid=1646432461&sprefix=samsun%2Caps%2C189&sr=8-2',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h20/h83/49792685178910/smartfon-samsung-galaxy-a23-sm-a235fzkkskz-128gb-black-104348541-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-a23-6-gb-128-gb-chernyi-104348541/?c=750000000#!/item',
     prodcategory: 2,
     shows: true,
     likes:0
   },
   {
     id: 9,
-    name: 'Samsung Galaxy S20',
-    price: 454,
-    description: 'Model name\tSamsung Galaxy S20 FE\n' +
+    name: 'Samsung Galaxy S22',
+    price: 1133,
+    description: 'Model name\tSamsung Galaxy S22 Ultra\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tSamsung\n' +
         'Form factor\tBar\n' +
-        'Memory storage capacity\t6 GB\n' +
-        'Operating system\tAndroid 11.0',
+        'Memory storage capacity\t12 GB\n' +
+        'Operating system\tAndroid 14.0',
     rating:4.5,
-    imageSrc: 'https://m.media-amazon.com/images/I/61GbkDVtLwL._AC_SL1100_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-Smartphone-Cloud-Android/dp/B096G5MZ34/ref=sr_1_18?crid=2ZF9UC4U7J8EN&keywords=samsung&qid=1646432550&sprefix=samsun%2Caps%2C189&sr=8-18',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h71/had/48695800168478/samsung-galaxy-s22-ultra-12-512gb-zelenyj-podarok-103671158-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-s22-ultra-12-gb-256-gb-zelenyi-103671158/?c=750000000#!/item',
     prodcategory: 2,
     shows: true,
     likes:0
   },
   {
     id: 10,
-    name: 'Samsung Galaxy S21',
-    price: 772,
-    description: 'Model name\tR9\n' +
+    name: 'Samsung Galaxy A52',
+    price: 504,
+    description: 'Model name\tSamsung Galaxy A52 256gb\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tSamsung\n' +
         'Form factor\tBar\n' +
-        'Memory storage capacity\t6 GB\n' +
-        'Operating system\tAndroid 12',
+        'Memory storage capacity\t8 GB\n' +
+        'Operating system\tAndroid 14',
     rating:4.4,
-    imageSrc: 'https://m.media-amazon.com/images/I/81u8oHzGLmL._AC_SL1500_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-Smartphone-Phantom-Android/dp/B08QX85YKM/ref=sr_1_17?crid=2ZF9UC4U7J8EN&keywords=samsung&qid=1646432550&sprefix=samsun%2Caps%2C189&sr=8-17',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he6/h59/49171847839774/samsung-galaxy-a52-8-256gb-cernyj-101198207-1-Container.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-a52-8-gb-256-gb-chernyi-101198207/?c=750000000#!/item',
     prodcategory: 2,
     shows: true,
     likes:0
   },
   {
     id: 11,
-    name: 'Samsung Galaxy A32',
-    price: 272,
-    description: 'Model name\tSamsung Galaxy A32\n' +
+    name: 'Samsung Galaxy S21 FE New',
+    price: 683,
+    description: 'Model name\tSamsung Galaxy S21 FE New\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tSamsung\n' +
         'Form factor\tTouchscreen\n' +
-        'Memory storage capacity',
+        'Memory storage capacity \t256 GB\n',
     rating:4.5,
-    imageSrc: 'https://m.media-amazon.com/images/I/71xetlaYOTL._AC_SL1500_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-Smartphone-128GB-Black/dp/B08W1V1226/ref=sr_1_11?crid=1OSCH0XCHFZXF&keywords=samsung&qid=1646857726&sprefix=samsung%2Caps%2C214&sr=8-11',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hf8/hf5/62234542407710/samsung-galaxy-s21-fe-new-6-gb-128-gb-cernyj-106336799-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-s21-fe-new-8-gb-256-gb-fioletovyi-106336799/?c=750000000#!/item',
     prodcategory: 2,
     shows: true,
     likes:0
   },
   {
     id: 12,
-    name: 'Samsung Galaxy A22',
-    price: 216,
-    description: 'Model name\tGalaxy A22\n' +
+    name: 'Samsung Galaxy A13',
+    price: 183,
+    description: 'Model name\tGalaxy A13\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tSamsung\n' +
         'Form factor\tSmartphone\n' +
         'Memory storage capacity\t64 GB',
     rating:4.5,
-    imageSrc: 'https://m.media-amazon.com/images/I/71zAlS6+6rL._AC_SL1500_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Samsung-Galaxy-A225-Black-Dual/dp/B0937JCNXT/ref=sr_1_2?crid=309KM17H57HSZ&keywords=samsung+galaxy+a22&qid=1646857796&sprefix=samsung+galaxy+a2%2Caps%2C173&sr=8-2',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3e/h3d/49613726941214/smartfon-samsung-galaxy-a13-64gb-sm-a135fzwvskz-white-104253316-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/samsung-galaxy-a13-4-gb-64-gb-belyi-104253316/?c=750000000#!/item',
     prodcategory: 2,
     shows: true,
     likes:0
   },
   {
     id: 13,
-    name: 'Xiaomi Poco M3 Pro',
-    price: 218,
-    description: 'Model name\tPoco M3 Pro 5G\n' +
+    name: 'Xiaomi 12S Ultra',
+    price: 1202,
+    description: 'Model name\tXiaomi 12S Ultra 12 GB/256 GB Black\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tXiaomi\n' +
         'Form factor\tTouchscreen\n' +
-        'Memory storage capacity\t4 GB',
-    rating:4.5,
-    imageSrc: 'https://m.media-amazon.com/images/I/518-LW36OCS._AC_SL1001_.jpg',
-    KaspiLink: 'https://m.media-amazon.com/images/I/518-LW36OCS._AC_SL1001_.jpg',
+        'Memory storage capacity\t12 GB',
+    rating:4.8,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hde/he2/63387595964446/xiaomi-12s-ultra-12-gb-256-gb-cernyj-106954956-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/xiaomi-12s-ultra-12-gb-256-gb-chernyi-106954956/?c=750000000#!/item',
     prodcategory: 3,
     shows: true,
     likes:0
   },
   {
     id: 14,
-    name: 'Xiaomi Poco X3',
-    price: 293,
-    description: 'Model name\tPoco X3\n' +
+    name: 'Xiaomi Black Shark 4 Pro',
+    price: 620,
+    description: 'Model name\tBlack Shark 4 Pro\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tXiaomi\n' +
         'Form factor\tBlock\n' +
-        'Memory storage capacity\t8 GB',
+        'Memory storage capacity\t12 GB',
     rating:4.6,
-    imageSrc: 'https://m.media-amazon.com/images/I/71fdWd4SqFL._AC_SL1380_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Xiaomi-Poco-X3-Pro-Smartphone/dp/B08Z84SB3R/ref=sr_1_15?crid=3RJ6OMFYB044C&keywords=xiaomi&qid=1646857971&sprefix=xiaomi%2Caps%2C173&sr=8-15&th=1',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h1b/he7/51399866646558/xiaomi-black-shark-4-pro-12-gb-256-gb-shadow-cernyj-105227587-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/xiaomi-black-shark-4-pro-12-gb-256-gb-shadow-chernyi-105227587/?c=750000000#!/item',
     prodcategory: 3,
     shows: true,
     likes:0
   },
   {
     id: 15,
-    name: 'Xiaomi Redmi Note 11',
-    price: 283,
-    description: '\n' +
+    name: 'Xiaomi Mi 11 Lite 5G NE',
+    price: 458,
+    description: 'Model name \tMi 11 Lite\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tXiaomi\n' +
         'Form factor\tTouchscreen\n' +
-        'Memory storage capacity\t128 GB\n' +
-        'Colour\tStar Blue',
+        'Memory storage capacity\t256 GB\n' +
+        'Colour\tBlack',
     rating:5,
-    imageSrc: 'https://m.media-amazon.com/images/I/51-JEJPrRDL._AC_SL1000_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Xiaomi-Smartphone-DotDisplay-Snapdragon-Camera-Star-Blue/dp/B09QYK466Y/ref=sr_1_2?crid=2NU0ZUOS1LL83&keywords=xiaomi+redmi+note+11&qid=1646857987&sprefix=xiaomi+redmi+note+11%2Caps%2C170&sr=8-2',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h05/h19/46571153784862/xiaomi-mi-11-lite-5g-ne-8-256gb-cernyj-102591493-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/xiaomi-mi-11-lite-5g-ne-8-gb-256-gb-chernyi-102591493/?c=750000000#!/item',
     prodcategory: 3,
     shows: true,
     likes:0
   },
   {
     id: 16,
-    name: 'Xiaomi Redmi Note 10',
-    price: 212,
-    description: 'Model name\tRedmi Note 10\n' +
+    name: 'Xiaomi Redmi Note 11 Pro',
+    price: 318,
+    description: 'Model name\tRedmi Note 11 Pro\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tXiaomi\n' +
-        'Form factor\tSchieberegler\n' +
+        'Form factor\tTouchscreen\n' +
         'Memory storage capacity\t128 GB',
     rating:4.6,
-    imageSrc: 'https://m.media-amazon.com/images/I/71CcXhAu9kS._AC_SL1500_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Xiaomi-Redmi-Note-10-Chrome/dp/B095BLF64H/ref=sr_1_1?crid=39JABBJIWFLUI&keywords=xiaomi%2Bremi%2Bnote%2B10&qid=1646858006&sprefix=xiaomi%2Bredmi%2Bnote%2B10%2Caps%2C171&sr=8-1&th=1',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hff/h0f/51166990336030/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386/?c=750000000#!/item',
     prodcategory: 3,
     shows: true,
     likes:0
   },
   {
     id: 17,
-    name: 'Xiaomi Redmi 9C',
-    price: 151,
-    description: 'Model name\tRedmi 9C\n' +
+    name: 'Xiaomi 12 Lite',
+    price: 442,
+    description: 'Model name\tXiaomi 12 Lite\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
         'Brand\tXiaomi\n' +
         'Form factor\tSmartphone\n' +
-        'Memory storage capacity\t3 GB\n' +
-        'Operating system\tAndroid 10.0',
+        'Memory storage capacity\t8 GB\n' +
+        'Operating system\tAndroid 14.0',
     rating:4.4,
-    imageSrc: 'https://m.media-amazon.com/images/I/81DvJbWniDL._AC_SL1500_.jpg',
-    KaspiLink: 'hhttps://www.amazon.de/-/en/MZB07Q0EU-Xiaomi-Redmi-9C-Smartphone/dp/B08DVHL2DV/ref=sr_1_1?crid=3RJ6OMFYB044C&keywords=xiaomi&qid=1646857971&sprefix=xiaomi%2Caps%2C173&sr=8-1&th=1',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha3/h02/61684710604830/xiaomi-12-lite-8-gb-256-gb-cernyj-106053690-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/xiaomi-12-lite-8-gb-256-gb-chernyi-106053690/?c=750000000#!/item',
     prodcategory: 3,
     shows: true,
     likes:0
   },
   {
     id: 18,
-    name: 'HTC Desire 500',
-    price: 326,
+    name: 'Huawei nova 10 SE ',
+    price: 342,
     description: 'Wireless carrier\tUnlocked\n' +
-        'Brand\tHTC\n' +
+        'Brand\tHuawei\n' +
         'Form factor\tTouchscreen\n' +
         'Memory storage capacity\t8 GB\n' +
-        'Operating system\tAndroid 4.2',
+        'Operating system\tAndroid 12.2',
     rating:3,
-    imageSrc: 'https://m.media-amazon.com/images/I/41j9oKsdLkL._AC_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Desire-Unlocked-Smartphone-Android-Jelly/dp/B00E4TLZ4I/ref=sr_1_4?keywords=htc%2Bsmartphone&qid=1646858423&sprefix=htc%2Bsmar%2Caps%2C158&sr=8-4&th=1',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd3/h5c/64494466269214/huawei-nova-10-se-8-gb-128-gb-zelenyi-podarok-huawei-y5p-i-selfie-stick-pro-107221521-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/huawei-nova-10-se-8-gb-128-gb-zelenyi-107221521/?c=750000000#!/item',
     prodcategory: 4,
     shows: true,
     likes:0
   },
   {
     id: 19,
-    name: 'HTC One M9',
-    price: 200,
-    description: 'Model name\tHTC One M9\n' +
+    name: 'Huawei Mate XS 2 Pala-L29F',
+    price: 1835,
+    description: 'Model name\tMate XS 2 Pala-L29F\n' +
         'Wireless carrier\tUnlocked\n' +
-        'Brand\tHTC\n' +
-        'Form factor\tStandard (Bar)\n' +
-        'Memory storage capacity\t32 GB',
-    rating:4.1,
-    imageSrc: 'https://m.media-amazon.com/images/I/71K4WAf9vnL._AC_SL1500_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/HTC-One-M9-Gold/dp/B00TX5QRAA/ref=sr_1_8?keywords=htc+smartphone&qid=1646858423&sprefix=htc+smar%2Caps%2C158&sr=8-8',
+        'Brand\tHuawei\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t512 GB',
+    rating:4.8,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h42/hcd/65034289774622/smartfon-huawei-mate-xs2-8-512gb-black-pala-l29f-107324368-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/huawei-mate-xs-2-pala-l29f-8-gb-512-gb-chernyi-107324368/?c=750000000#!/item',
     prodcategory: 4,
     shows: true,
     likes:0
   },
   {
     id: 20,
-    name: 'HTC Desire 21 Pro',
-    price: 319,
-    description: 'Model name\tDesire 21 Pro 5G\n' +
+    name: 'Huawei P50 Pocket',
+    price: 1491,
+    description: 'Model name\tP50 Pocket\n' +
         'Wireless carrier\tUnlocked for All Carriers\n' +
-        'Brand\tHTC\n' +
-        'Form factor\tStoßabsorbierend\n' +
-        'Memory storage capacity\t6 GB',
+        'Brand\tHuawei\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t 512 GB',
     rating:4.2,
-    imageSrc: 'https://m.media-amazon.com/images/I/41n3VHQBxtL._AC_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/HTC-Desire-21-Pro-5G-blue/dp/B092JP9YWH/ref=sr_1_1?keywords=htc+smartphone&qid=1646858384&sprefix=htc+smar%2Caps%2C158&sr=8-1',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hde/h20/52237746634782/huawei-p50-pocket-12-gb-512-gb-zolotistyj-105680401-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/huawei-p50-pocket-12-gb-512-gb-zolotistyi-105680401/?c=750000000#!/item',
     prodcategory: 4,
     shows: true,
     likes:0
   },
   {
     id: 21,
-    name: 'HTC Desire X',
-    price: 104,
-    description: 'Model name\tX\n' +
-        'Wireless carrier\tNET\n' +
-        'Brand\tHTC\n' +
+    name: 'Huawei Mate 50 Pro',
+    price: 965,
+    description: 'Model name\tHuawei Mate 50 Pro\n' +
+        'Wireless carrier\tUnlocked for All Carriers\n' +
+        'Brand\tHuawei\n' +
         'Form factor\tTouchscreen\n' +
-        'Memory storage capacity\t4096 MB',
-    rating:3.7,
-    imageSrc: 'https://m.media-amazon.com/images/I/71vK8U+4yaL._AC_SL1456_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Desire-Smartphone-Android-Sense-Bluetooth/dp/B009GGY852/ref=sr_1_2?keywords=htc+smartphone&qid=1646858384&sprefix=htc+smar%2Caps%2C158&sr=8-2',
+        'Memory storage capacity\t256 GB',
+    rating:4.7,
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h86/h22/65587221397534/mate-50-pro-8-256gb-elegantnyi-chernyi-dco-lx9-107529187-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/huawei-mate-50-pro-8-gb-256-gb-chernyi-107529187/?c=750000000#!/item',
     prodcategory: 4,
     shows: true,
     likes:0
   },
   {
     id: 22,
-    name: 'HTC Desire 816',
-    price: 157,
-    description: 'Model name\tHTC Desire 816\n' +
-        'Brand\tHTC\n' +
-        'Form factor\tStandard (Bar)\n' +
-        'Memory storage capacity\t8 GB\n' +
-        'Operating system\tAndroid 4.4\n' +
-        'Colour\tWeiß',
+    name: 'Huawei Nova Y90',
+    price: 252,
+    description: 'Model name\tHuawei Nova Y90\n' +
+        'Brand\tHuawei\n' +
+        'Form factor\tTouchscreen\n' +
+        'Memory storage capacity\t128 GB\n' +
+        'Operating system\tAndroid 14\n' +
+        'Colour\tlight blue',
     rating:4.6,
-    imageSrc: 'https://m.media-amazon.com/images/I/61oiYM+h4gL._AC_SL1000_.jpg',
-    KaspiLink: 'https://www.amazon.de/-/en/Smartphone-Megapixel-Touchscreen-Quad-Core-Processor/dp/B00ISUSB3M/ref=sr_1_3?keywords=htc+smartphone&qid=1646858384&sprefix=htc+smar%2Caps%2C158&sr=8-3',
+    imageSrc: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h5d/h53/61766054543390/huawei-nova-y90-4-gb-128-gb-goluboj-106096409-1.jpg',
+    KaspiLink: 'https://kaspi.kz/shop/p/huawei-nova-y90-4-gb-128-gb-goluboi-106096409/?c=750000000#!/item',
     prodcategory: 4,
     shows: true,
     likes:0
